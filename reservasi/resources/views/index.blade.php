8115ab3b03d000ea6c9c0fa59acca942f2fc8246
Donaldson Sitorus 09020182327004 <donalsitorus720@gmail.com>
2025-04-14 04:46:06 +0000

    Instalasi Laravel

---
+++ reservasi/resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Beranda</title>
+</head>
+<body>
+    <h1>Selamat Datang di Website Kami</h1>
+
+    <p>Ini adalah halaman utama.</p>
+
+    <nav>
+        <ul>
+            <li><a href="/">Beranda</a></li>
+            <li><a href="/login">Login</a></li>
+            <li><a href="/about">Tentang</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
